4029e32f89d4502e6ddd6579629cc43be7f6a33b
Vladimir Surov <vovkasu@gmail.com>
2018-07-10 23:03:07 +0300

    Restart scene on Game Over;

---
+++ Assets/Eagle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class Eagle : MonoBehaviour
+{
+    public SpriteRenderer View;
+
+    public Sprite DieView;
+    public event EventHandler OnDie;
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.layer != LayerMask.NameToLayer("Bullet")) return;
+        View.sprite = DieView;
+        if (OnDie != null)
+        {
+            OnDie(this, null);
+        }
+    }
+
+}
---
+++ Assets/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public Transform PlayerSpawnPosition;
+    public TankController TankControllerPrefab;
+    public Eagle Eagle;
+    public Transform GameRooTransform;
+
+    private void Start()
+    {
+        var tankController = Instantiate(TankControllerPrefab, GameRooTransform);
+        tankController.transform.position = PlayerSpawnPosition.position;
+        Eagle.OnDie += (sender, args) => OnEagleDie();
+    }
+
+    private void OnEagleDie()
+    {
+        StartCoroutine(RestartLevel());
+    }
+
+    private IEnumerator RestartLevel()
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
