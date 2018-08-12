0b348f2b38964abc6a9e09afcb5fe17b878f6f2a
Vladimir Surov <vovkasu@gmail.com>
2018-08-13 00:39:16 +0300

    Create Emeny/Level models

---
+++ Assets/EnemyModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [CreateAssetMenu(fileName = "TankModel", menuName = "BattleCity/TankModel", order = 2)]
+    public class EnemyModel : ScriptableObject
+    {
+        public float Speed;
+        public float BulletSpeed;
+
+        public List<TankView> Views;
+    }
+
+    [Serializable]
+    public class TankView
+    {
+        public string Name;
+        public Sprite Sprite;
+    }
+
+
+}
---
+++ Assets/LevelModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using DefaultNamespace;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Level", menuName = "BattleCity/Level", order = 1)]
+public class LevelModel : ScriptableObject
+{
+    public List<EnemyModel> Enemies = new List<EnemyModel>();
+}
